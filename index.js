73b39fb2d20e6ef32e7d9869b0773f9f18b57a2b
Jose Santos <jcv.santos229@gmail.com>
2020-07-01 05:01:27 +1200

    hid token and fixed config

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const config = require('../config.json');
+const PREFIX = 'timm.';
 
 bot.on('ready', ()  =>{
     console.log('This bot is online!');
 })
 
 bot.on('message', message=>{
-    let args = message.content.substring(config.prefix.length).split('.');
+    let args = message.content.substring(PREFIX.length).split('.');
 
     switch(args[0]){
         case 'sayHello();':
